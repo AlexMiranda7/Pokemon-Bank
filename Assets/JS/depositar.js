1391ddecbc72b8e4d764e60d1910cdec02a45de6
Alexander Miranda <alex.mirandau2018@gmail.com>
2020-10-04 21:40:18 -0600

    Cambio pequeños en deposito

---
+++ Assets/JS/depositar.js
@@ -0,0 +1,4 @@
+import swal from 'sweetalert';
+function prueba() {
+   swal("Good job!", "You clicked the button!", "success");
+}
---
+++ Assets/depositar.js
@@ -0,0 +1,5 @@
+import swal from 'sweetalert';
+
+function prueba() {
+    swal("Good job!", "You clicked the button!", "success");
+}
